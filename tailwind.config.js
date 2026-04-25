77cbe132fdf9b5cae45bdc6ab20c957c191d3f10
ugdcx <125936046+ugdcx@users.noreply.github.com>
2026-04-25 11:50:07 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        cream: "#F5F0E8",
+        stoneDark: "#1F1D1A",
+        gold: "#B89B5E"
+      }
+    }
+  },
+  plugins: [],
+};
